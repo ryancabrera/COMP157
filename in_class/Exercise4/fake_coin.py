aa0ac398af37f02ea7db7955ba83c449c9e945d2
Ryan Cabrera <r_cabrera1@u.pacific.edu>
2016-05-27 11:00:12 -0700

    Added more in-class exercises

---
+++ in_class/Exercise4/fake_coin.py
@@ -0,0 +1,17 @@
+__author__ = 'Ryan Cabrera'
+
+
+def main():
+    coin_stack = [2] * 8
+    coin_stack.append(1)
+
+    print(coin_stack)
+    divide_stack(coin_stack, 3)
+
+
+def divide_stack(stack_of_coins, n_stacks):
+    print()
+
+
+if __author__:
+    main()
